883db784735bd665d63d8c79c5417453b1e5ee65
Scott Eisele <eiselesr@gmail.com>
2023-10-12 13:39:47 -0500

    remove poller

--- RELAYF1.py
+++ RELAYF1.py
@@ -30,11 +30,6 @@ class RELAYF1(ModbusDeviceComponent):
         self.send_modbus(modbus_msg)
     # riaps:keep_device_port:end
 
-    # riaps:keep_poller:begin
-    def on_poller(self):
-        now = self.poller.recv_pyobj()
-    # riaps:keep_poller:end
-
     # riaps:keep_modbus_cmd_port:begin
     def on_modbus_command_port(self):
         # Receive response from modbus device
