dd65650b499c50f428bf10aa7e62f14d680dcea7
Scott Eisele <eiselesr@gmail.com>
2024-04-19 12:25:32 -0500

    add potential devices as comments

--- 2024_4_11_milestone/op_set.py
+++ 2024_4_11_milestone/op_set.py
@@ -16,10 +16,13 @@ def main():
     P_map = {
         "F1_DSP111": 100,
         "F1_DSP112": 200,
+        # "F3_DSP115": 300,
+        # "F3_DSP116": 400,
         "GEN1-Banshee": 1000,
+        "GEN2-Banshee": 1000,
+        "GEN3-Banshee": 1000,
     }
 
-   
     PQ_map = {
         key: {"P": value, "Q": value * math.tan(math.acos(COS_PHI))}
         for key, value in P_map.items()
